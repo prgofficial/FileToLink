fa936fc8ab47bd401b1f1adcdfcfc9027a8aa129
ViruZs <52099763+TGExplore@users.noreply.github.com>
2020-05-11 11:26:08 +0530

    Fixed (  ) files to links problem

--- tgfilestream/telegram.py
+++ tgfilestream/telegram.py
@@ -44,6 +44,6 @@ async def handle_message(evt: events.NewMessage.Event) -> None:
         await evt.reply(start_message)
         return
     url = public_url / str(pack_id(evt)) / get_file_name(evt)
-    await evt.reply(f"Link to download file: [{url}]({url})")
+    await evt.reply(f"Link to download file: {url}")
     log.info(f"Replied with link for {evt.id} to {evt.from_id} in {evt.chat_id}")
     log.debug(f"Link to {evt.id} in {evt.chat_id}: {url}")
